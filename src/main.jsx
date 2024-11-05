445ad94dc7af221fac8783ffb999637eff9c80f8
mehediNabil <mehedinabil13@gmail.com>
2024-11-05 09:05:27 +0600

    project initalization

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root.jsx';
+import ErrorPage from './Components/ErrorPage.jsx';
+import Home from './Components/Home.jsx';
+import DashBoard from './Components/DashBoard.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+      {
+        path: 'dashboard',
+        element: <DashBoard></DashBoard>
+
+      }
+
+    ]
+
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+
+  </StrictMode>,
+)
+
+
